5b870268ffaab0164abf6d3e016243893a3bd421
Shanu <simplyshanu@outlook.com>
2024-02-20 14:59:25 -0500

    adding search icon

--- src/app/components/search.jsx
+++ src/app/components/search.jsx
@@ -1,5 +1,14 @@
 "use client";
-import { Input, Card, CardBody, Avatar, Text } from "@chakra-ui/react";
+import {
+  Input,
+  Card,
+  CardBody,
+  Avatar,
+  Text,
+  InputLeftElement,
+  InputGroup,
+} from "@chakra-ui/react";
+import { SearchIcon } from "@chakra-ui/icons";
 import { useEffect, useState } from "react";
 import Style from "@/app/styles/navBar.module.css";
 
@@ -20,7 +29,12 @@ export default function Search() {
 
   return (
     <Card className={Style.select}>
-      <Input m={5} mb={2} width={265} placeholder="search user..."></Input>
+      <InputGroup m={5} mb={2} width={265}>
+        <InputLeftElement pointerEvents="none">
+          <SearchIcon color="gray.400" />
+        </InputLeftElement>
+        <Input placeholder="search user..."></Input>
+      </InputGroup>
       <CardBody display="flex" flexDirection="column" pt={0}>
         {users.map((user) => {
           return (
